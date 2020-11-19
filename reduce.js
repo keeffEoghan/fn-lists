788797631be096cfad7319e8eda822cf080a513c
Eoghan O'Keeffe <keeffeoghan@gmail.com>
2020-11-19 13:54:37 +0000

    Comments

--- reduce.js
+++ reduce.js
@@ -3,17 +3,16 @@ const r = Array.prototype.reduce;
 /**
  * Reduce an array-like object.
  * Similar to native, but with iteratee-first arguments.
- * Supports the native one-value behaviour.
+ * Supports the native single-argument behaviour.
  *
- * Needs a fully separate `call` because native arrays detect an `undefined`
- * parameter.
- *
- * @param {function} f The iteratee function, given standard arguments.
+ * @param {function} f The iteratee function, given standard arguments, plus the
+ *     `out` argument.
  * @param {array} a The array operand.
  * @param {*} [out] The initial accumulator, if given; otherwise, uses standard
  *     `Array.reduce` behaviour.
  */
 export const reduce = (f, a, out) =>
+    // Needs separate `call` because native detects even `undefined` parameter.
     ((out === undefined)? r.call(a, f) : r.call(a, f, out));
 
 export default reduce;
