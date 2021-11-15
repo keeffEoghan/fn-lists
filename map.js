2282d772995ea0d3bc3cf2e01bdc066a050e1f95
Eoghan O'Keeffe <keeffeoghan@gmail.com>
2021-11-15 16:26:27 +0000

    ES and CommonJS exports, new Parcel/Yarn

--- map.js
+++ map.js
@@ -9,11 +9,11 @@ import { reduce } from './reduce';
  * @param {function} f The iteratee function, given standard `Array.map`
  *     arguments, plus the output object.
  * @param {array} a The list to map over (array or array-like object).
- * @param {*} [out=[]] The initial accumulator, if given; `a`, if defined and
- *     falsey.
+ * @param {*} [out=[]] The initial accumulator, if given; `a` if falsey given;
+ *     or a new array if not given.
  *
- * @returns {*} The `out` object, having been mapped through the iteratee
- *     function over the list.
+ * @returns {*} The result of mapping `out` through the iteratee function over
+ *     the `a` list.
  */
 export const map = (f, a, out = []) => reduce((out, v, i) => {
         out[i] = f(v, i, a, out);
--- range.js
+++ range.js
@@ -1,10 +1,16 @@
 /**
- * Creates an iterable array of the given length, containing empty entries.
+ * Creates an iterable array of the given length of empty entries, or
+ * those given by optional arguments to `Array.fill`.
+ *
+ * @see Array.fill
  *
  * @param {number} n The number of elements to create.
+ * @param {*} [value] A value to fill the array with; for `Array.fill`.
+ * @param {number} [start=0] Start index, inclusive; for `Array.fill`.
+ * @param {number} [end=n] End index, exclusive; for `Array.fill`.
  *
  * @returns {array} An array of the given number of empty elements.
  */
-export const range = (n) => Array(n).fill();
+export const range = (n, value, start, end) => Array(n).fill(value, start, end);
 
 export default range;
